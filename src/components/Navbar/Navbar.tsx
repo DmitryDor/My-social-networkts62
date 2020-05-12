34c2e0ba7c8ac2538a273c7098e6232368a49d00
DmitryDor <dmitrydoroshenko80@gmail.com>
2020-05-12 18:22:59 +0300

    Lesson20(NaveLink)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import styles from "./Navbar.module.css";
+import {NavLink} from "react-router-dom";
+
+
 
 const Navbar = () => {
     return (
         <nav className={styles.nav}>
 
             <div className={styles.item}>
-                <a href = "/profile"> Profile</a>
+                <NavLink to = "/profile" activeClassName={styles.activeLink}> Profile</NavLink>
             </div>
-            <div className={`${styles.item} ${styles.active}`}>
-                <a href= "/dialogs"> Messages</a>
-            </div>
-            <div className={styles.item}>
-                <a href= "/news"> News</a>
+            <div className={`${styles.item} ${styles.activeLink}`}>
+                <NavLink to = "/dialogs" activeClassName={styles.activeLink}> Messages</NavLink>
             </div>
             <div className={styles.item}>
-                <a href= "/music"> Music</a>
+                <NavLink to = "/news" activeClassName={styles.activeLink}> News</NavLink >
+            </div>
+            <div className={styles.item}>
+                <NavLink to= "/music" activeClassName={styles.activeLink}> Music</NavLink>
             </div >
             <div className={styles.item}>
-                <a href= "/settings"> Settings</a>
+                <NavLink to= "/settings" activeClassName={styles.activeLink}> Settings</NavLink>
             </div>
 
         </nav>
